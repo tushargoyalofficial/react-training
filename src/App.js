991d60d80a522c17b77806415809248d54952bcf
Bunny Molester <tushar462024@gmail.com>
2019-01-05 21:46:37 +0530

    Implement input field and create functionality to handle inputed value

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: ""
+    };
+
+    this.handleInputs = this.handleInputs.bind(this);
+    this.manipulateInput = this.manipulateInput.bind(this);
+  }
+
+  handleInputs(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  manipulateInput(index) {
+    let n = this.state.name;
+    n = n.split("");
+    n.splice(index, 1);
+    this.setState({ name: n.join("") });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header" />
+        <header className="App-header">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputs}
+          />
+
+          <br />
+        </header>
       </div>
     );
   }
